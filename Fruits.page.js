4961ff1357fe85ff5b801d97404f225ef6edf587
captainalabi <52130306+captainalabi@users.noreply.github.com>
2023-02-14 00:39:03 +0100

    Add files via upload

--- Fruits.page.js
+++ Fruits.page.js
@@ -2,14 +2,20 @@ import Page from "./page.js";
 
 class Fruits extends Page{
 
-//sync addToCartButton(index){return $(`#root div.products-wrapper div:nth-child(${index}) div.product-action button`)}
+// get any addToCart Button by index. Note that buttonIndex = productIndex
+/**
+ * 
+ * @param {number} buttonIndex 
+ * @returns button selector for the index
+ */
+ addTocartButton (buttonIndex) {
+     return $(`div:nth-child(${buttonIndex}) div.product-action `)
+    }
 
-get brocolliButton (){return $("#root div.products-wrapper div:nth-child(1) div.product-action button")}
-get cauliflowerButton (){ return $('#root div.products-wrapper div:nth-child(2) div.product-action button')}
-get cucumberButton (){ return $('#root div.products-wrapper div:nth-child(3) div.product-action button')}
-get beetrootButton (){ return $('#root div.products-wrapper div:nth-child(4) div.product-action button')}
-
+    //get the search input field
 get searchField(){ return $(".search-keyword")}
+
+get allProductLabels (){ return $$(".product-name")}
 
 /**
  * set value of the search field to search for product
@@ -19,103 +25,43 @@ async searchForProducts(productName){
     await this.searchField.setValue(productName)
  }
 
- /*
-async addToCart(productsArray){
- 
-    for(let i = 1; i < productsArray.length; i++){
-
-        await this.searchForProducts(productsArray[i])
-        await this.addToCartButton(i).click()
-    }
-}
-*/
- /**
- * click add to cart button to add the product searched to cart
+/**
+ * search for the index of any named product
+ * @param {string} name of product to buy
+ * @returns {number} index of the product
  */
 
- /*
-async specifyNoAndBuyBrocolli(NoToBuy){
-    
-    for(let i = 1; i <= NoToBuy; i++){
-       await this.addBrocolliToCart()
-        if(i === NoToBuy){
-           break;
+
+/**
+ * 
+ * @param {string} productName 
+ * @param {number} howMany 
+ */
+async clickMultipleTimes(productName, howMany){
+
+
+
+    for(let j = 1; j <= howMany; j++ ){
+        
+        for(let productIndex = 1; productIndex <= await this.allProductLabels.length; ++productIndex){
+        
+            //productIndex ++
+            //if text contains name of fruit, return index
+            if( ((await this.addTocartButton (productIndex ).getText()).includes(productName))){
+                
+                console.log( "productIndex ::::::::::::: " + productIndex)
+                console.log( "productName ::::::::::::: " + await this.addTocartButton (productIndex ).getText())
+                console.log( "howMany ::::::::::::: " + howMany)
+                
+               await this.addTocartButton(productIndex ).click()
+
+               break;
+            }
         }
-    }
-    
-}
-
-async specifyNoAndBuyCauliflower(NoToBuy){
-    
-    
-      setInterval( await this.addCauliflowerToCart(), 2000)
        
-       
-     }
-    
-    
-
-
-async specifyNoAndCucumber(NoToBuy){
-    
-    for(let j = 1; j <= NoToBuy; j++){
-       await this.addCucumberToCart()
-       if(j === NoToBuy){
-        break;
-     }
     }
-    
 }
 
-async specifyNoAndBuyBeetroot(NoToBuy){
-    
-    for(let j = 1; j <= NoToBuy; j++){
-       await this.addBeetrootToCart()
-       if(j === NoToBuy){
-        break;
-     }
-    }
-    
-}
-
-async addBrocolliToCart(NoToBuy){
-    let i =0;
- let timer =  setInterval( async function(){  for(let j = 1; j <= NoToBuy; j++){
-i++
-    await this.searchForProducts('Brocolli')
-    await  this.brocolliButton.click()
-    await  this.searchField.clearValue()
- }
-    }, 100000)
-   if(i === NoToBuy){
-    clearInterval(timer)
-   }
-}
-*/
-async addBrocolliToCart(){
-    await this.searchForProducts('Brocolli')
-    await this.brocolliButton.click()
-    await this.searchField.clearValue()
-}
-
-async addCauliflowerToCart(){
-    await this.searchForProducts('Cauliflower')
-    await this.cauliflowerButton.click()
-    await this.searchField.clearValue()
-}
-
-async addCucumberToCart(){
-    await this.searchForProducts('Cucumber')
-    await this.cucumberButton.click()
-    await this.searchField.clearValue()
-}
-
-async addBeetrootToCart(){
-    await this.searchForProducts('Beetroot')
-    await this.beetrootButton.click()
-    await this.searchField.clearValue()
-}
- 
 async open(){
     return super.open()
 }
--- Fruits.test.js
+++ Fruits.test.js
@@ -4,28 +4,38 @@ import FruitsPage from "../pageobjects/Fruits.page.js";
 describe('Test ecommerce', () => {
     it('buy 4 fruits', async () => {
         
-       await FruitsPage.open()
+      await browser.url("https://rahulshettyacademy.com/seleniumPractise/#/");
+
+      //get the index from product labels
+      let index = 0 ;
+
+     await FruitsPage.allProductLabels.forEach( element => {
+        index ++         
+
+        if(FruitsPage.allProductLabels[index].getText().includes("Brocolli")){
+          return;
+        }
+      });
+
+      // click the button in no of times
+      let howMany = 3
+      //let index = 1
+
+      for(let i =1; i <= howMany; i++){
+      await FruitsPage.addTocartButton(index).click()
+      }
+
+
+       //await FruitsPage.open()
        
-       await browser.maximizeWindow()
-        /*
-       await FruitsPage.addBrocolliToCart(2)
-        
-       await FruitsPage.specifyNoAndBuyCauliflower(1)
+       //await browser.maximizeWindow()
+ /*     
+let productName = "Brocolli"
+let howMany = 3
 
-       await FruitsPage.specifyNoAndBuyBeetroot(1)
-
-       await FruitsPage.specifyNoAndCucumber(1)
-        */
-       /*
-       let arrayOfFruits = [ "Brocolli", "Cauliflower", "Cucumber", "Beetroot"]
-      await FruitsPage.addToCart(arrayOfFruits)
-*/
-       
-     await FruitsPage.addBrocolliToCart()
-     await FruitsPage.addCauliflowerToCart()
-     await FruitsPage.addBeetrootToCart()
-     await FruitsPage.addCucumberToCart()
-
+       //add 3 Brocolli to cart
+      await FruitsPage.clickMultipleTimes(productName, howMany)
+       */
       await browser.pause(6000)
     });
 });
